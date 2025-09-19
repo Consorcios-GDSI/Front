b9b9ee65284599d05ebf30426d334dd86958a973
LuciaAlmanza <luciaalmanzaa@gmail.com>
2025-09-19 14:05:12 -0300

    iconos

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,12 +1,25 @@
-import { FaUserCircle } from "react-icons/fa";
+import { FaUserCircle, FaHome } from "react-icons/fa";
 
 function Header() {
   return (
     <header className="header">
       <h1>Consorcios</h1>
+
+      {/* Menú horizontal */}
+      <nav className="header-menu">
+        <a href="/" className="menu-item">
+          <FaHome size={18} /> Home
+        </a>
+        <a href="/reportes" className="menu-item">Reportes</a>
+        <a href="/gastos" className="menu-item">Gastos</a>
+        <a href="/expensas" className="menu-item">Expensas</a>
+        <a href="/propietarios" className="menu-item">Gestión de Propietarios</a>
+        <a href="/pagos" className="menu-item">Pagos</a>
+      </nav>
+
       <div className="user-info">
         <span>Franco</span>
-        <FaUserCircle size={32} />
+        <FaUserCircle size={28} />
       </div>
     </header>
   );
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -4,8 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import Counter from "./counter";
 import "./style.css";
 
-const root = document.getElementById("root");
-if (!root) throw new Error("Root element not found");
+const root = document.getElementById("app");
+if (!root) throw new Error("App element not found");
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
@@ -14,3 +14,4 @@ ReactDOM.createRoot(root).render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
